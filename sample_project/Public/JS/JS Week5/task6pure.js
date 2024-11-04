2b5adcff2b9ee4c07573198bc0b553faa852f6a6
HomelyZacombie <91287052+HomelyZacombie@users.noreply.github.com>
2024-11-04 09:06:44 +0000

    forgotten commit

--- sample_project/Public/JS/JS Week5/task6pure.js
+++ sample_project/Public/JS/JS Week5/task6pure.js
@@ -1,5 +1,5 @@
 function getSum(accumulator, item){
-    return accumulator += parseFloat(item.value);
+    return accumulator += parseFloat(item.value)
 }
 
 
@@ -8,10 +8,7 @@ function totup(myForm){
     let total = 0;
     entries = Array.from(myForm.elements.entry);
     total = entries.reduce(getSum,0);
-    total = total * (1 - (myForm.elements.d1.value/100))
+    total = ( myForm.elements.entry[1].value)
     
-    console.log (myForm.elements.entry);
-    console.log (entries.join());
-
     document.getElementById("totalled").innerHTML = total.toFixed(2);
 }
